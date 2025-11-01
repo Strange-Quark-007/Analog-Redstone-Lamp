ba84f5c095ea23a13ae14f3594bba85cab5de320
Strange-Quark-007 <masteryasin@ymail.com>
2025-11-01 23:16:24 +0530

    Initial project setup for multi-loader minecraft mod

---
+++ common/src/main/java/strangequark/analogredstonelamp/Constants.java
@@ -0,0 +1,11 @@
+package strangequark.analogredstonelamp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "analogredstonelamp";
+	public static final String MOD_NAME = "AnalogRedstoneLamp";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
